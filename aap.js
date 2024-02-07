97de7d2a1ae552a88db94cf87392df683ac4b252
Muhammadizhan <izhanayaz4@gmail.com>
2024-02-07 20:00:09 +0500

    intro javascript

---
+++ aap.js
@@ -0,0 +1,20 @@
+var a = 10;
+var b = 20;
+console.log(a);
+
+var firstName = "Muhammad Izhan";
+var age = 20;
+var isUser ="false"
+
+console.log(firstName);
+console.log(age);
+console.log(isUser);
+
+var firstName = "Izhan";
+var lastName = "Ayaz";
+var isUser = false;
+
+console.log(typeof(lastName))
+console.log(firstName , age ,isUser);
+console.log("my name is " + firstName + "and my age is" + ''+ age);
+
